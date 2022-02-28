ddf26a4b374e0dc369385904174a5ec69b15b0ce
Fogha <collinslord50@gmail.com>
2022-03-01 00:28:31 +0100

    setting up portfolio code structure, building navbar component

---
+++ ui/src/containers/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../Header/Header'
+import './Footer.scss'
+
+export const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Header
---
+++ ui/src/containers/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Header.scss'
+import { motion } from 'framer-motion'
+import { images } from '../../shared'
+
+const Header = () => {
+  return (
+    <div className="app__header app__flex">
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+
+          </div>
+
+        </div>
+      </motion.div>
+    </div>
+  )
+}
+
+export default Header
